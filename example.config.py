d5727da888d6d88f4de6c31b37671c6709c57ac4
tom29739 <tom29739@users.noreply.github.com>
2016-10-31 23:37:40 +0000

    Create example config.py file.

---
+++ example.config.py
@@ -0,0 +1,11 @@
+# config.py
+# This is an example config file for ircredirector.
+# To use it, fill in your application's information and rename this file to `config.py`.
+
+### THIS FILE NEEDS TO BE KEPT SECURELY ###
+
+# consumer_key
+comsumer_key = "Replace me with your OAuth consumer key"
+
+# consumer_secret
+comsumer_secret = "Replace me with your OAuth consumer secret"
